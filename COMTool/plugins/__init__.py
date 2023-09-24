ad4d3e6cd7d825819990b8227e344f50e21c3a04
StrangeChen <15201901850@163.com>
2023-09-25 01:23:50 +0800

    ENH: show function widget and add rpc log file

--- COMTool/plugins/__init__.py
+++ COMTool/plugins/__init__.py
@@ -2,9 +2,10 @@ from . import dbg
 from . import protocol
 from .import terminal
 from . import graph
+from . import rpc_dbg
 # from . import myplugin
 
-pluginClasses = [dbg.Plugin, protocol.Plugin, terminal.Plugin, graph.Plugin]
+pluginClasses = [rpc_dbg.Plugin, dbg.Plugin, protocol.Plugin, terminal.Plugin, graph.Plugin]
 # pluginClasses.append(myplugin.Plugin)
 
 builtinPlugins = {}
